6aedbe1bb15d4bb70f5dd6e0018f891b46f322c2
Maria Eduarda <ma.eferreira20@gmail.com>
2025-08-05 20:38:06 -0300

    adicionando rodapé

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Titulo from './Titulo';
+import Rodape from './Rodape'
 
 function App() {
-  
+
 
   return (
     <>
       <div cLassName='container'>
         <Titulo />
       </div>
+      <Rodape />
     </>
   )
 }
---
+++ src/Rodape.jsx
@@ -0,0 +1,19 @@
+import './Rodape.css'
+
+function Rodape() {
+    return (
+        <>
+        <footer className='text-center bg-dark text-white p-3'>
+            <p>
+                Desenvolvido por &nbsp;
+                <a herf='https://linkedin.com/in/mariaeduarda'
+                target ='blank' className='text-danger'>Maria Eduarda</a>
+                &nbsp; no &nbsp;
+                <a herf='https;//sp.senac.br/americana'
+                taeget='blank' className='text-danger'>Senac Americana</a>
+            </p>
+        </footer>
+        </>
+    )
+}
+export default Rodape
