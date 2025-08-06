bf031b4a49d9473965f47d0d2520dc9890775255
Maria Eduarda <ma.eferreira20@gmail.com>
2025-08-05 21:45:06 -0300

    adicionando menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.css'
-import Titulo from './Titulo';
+import Titulo from './Titulo'
 import Rodape from './Rodape'
+import Menu from './Menu'
 
 function App() {
 
@@ -10,8 +12,10 @@ function App() {
     <>
       <div cLassName='container'>
         <Titulo />
+        <Menu />
       </div>
       <Rodape />
+
     </>
   )
 }
---
+++ src/Menu.jsx
@@ -0,0 +1,28 @@
+import  './Menu.css'
+
+function Menu() {
+    return (
+        <>
+        <nav className="navbar navbar-expand-lg navbar-ligth bg-light">
+            <button
+            className="navbar-toggler m-auto"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            >
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav m-auto text-center">
+                    <li className="nav-item">
+                        <a className="nav-link" herf="#">
+                            Cauculadora Dólar
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+}
+export default Menu
